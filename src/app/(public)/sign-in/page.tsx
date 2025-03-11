811703a2e04c3c904a85569c6183d69fb30317f5
Hyojoon Park <hpar9758@naver.com>
2025-03-12 05:59:25 +1100

    feat: refactor global layout to client component

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center flex-grow bg-pink-50">
+    <div className="flex flex-col items-center justify-center flex-grow">
       <Button>Sign in with Google</Button>
       <Button onClick={signInWithGithub}>Sign in with Github</Button>
     </div>
--- src/components/layouts/global-layout.tsx
+++ src/components/layouts/global-layout.tsx
@@ -1,25 +1,32 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { signOut } from "@/actions/auth.action";
-import { createClient } from "@/utils/supabase/server";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
-export default async function GlobalLayout({
+export default function GlobalLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const [user, setUser] = useState<unknown>(null);
+
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      setUser(user);
+    });
+  }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
       {/* Header */}
       <header className="sticky top-0 left-0 py-3 px-4 sm:px-16 border-b z-1 bg-white border-black/10 flex items-center justify-between">
         <Link
-          href={"/"}
+          href={`${user ? "/my-workspace" : "/"}`}
           className="flex space-x-2 items-center text-lg font-extrabold tracking-tighter hover:cursor-pointer"
         >
           <Image
