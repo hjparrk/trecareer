ea5bccfbf47b5f266dd3e3215bea5022be1a0aea
Hyojoon Park <hpar9758@naver.com>
2025-03-12 06:39:51 +1100

    chore: fix oauth button rounded

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -2,6 +2,35 @@ import { signInWithGithub } from "@/actions/auth.action";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+function ProviderButton({
+  signIn,
+  provider,
+  src,
+}: {
+  signIn: () => Promise<void>;
+  provider: string;
+  src: string;
+}) {
+  return (
+    <Button
+      onClick={signIn}
+      asChild
+      className="py-6 hover:cursor-pointer px-2 rounded-4xl"
+    >
+      <div>
+        <Image
+          src={src}
+          alt={provider}
+          width={0}
+          height={0}
+          className="w-8 h-8"
+        />
+        <h1 className="text-base">Sign in with {provider}</h1>
+      </div>
+    </Button>
+  );
+}
+
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center flex-grow gap-10">
@@ -10,19 +39,16 @@ export default function Page() {
         <h1>Please Sign In.</h1>
       </header>
       <div className="flex flex-col gap-5">
-        <Button>Sign in with Google</Button>
-        <Button onClick={signInWithGithub} asChild className="py-6">
-          <div>
-            <Image
-              src="/github-mark.svg"
-              alt="github mark"
-              width={0}
-              height={0}
-              className="w-8 h-8"
-            />
-            <h1 className="text-base">Sign in with Github</h1>
-          </div>
-        </Button>
+        <ProviderButton
+          signIn={signInWithGithub}
+          provider="Google"
+          src="/google.svg"
+        />
+        <ProviderButton
+          signIn={signInWithGithub}
+          provider="Github"
+          src="/github.svg"
+        />
       </div>
     </div>
   );
