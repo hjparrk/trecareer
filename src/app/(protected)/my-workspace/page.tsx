9dbbfd6ea6e4e085f4d3771ccd9b1a3d05c2bbe2
Hyojoon Park <hpar9758@naver.com>
2025-03-12 18:46:57 +1100

    fix: prevent event propagation

--- src/app/(protected)/my-workspace/page.tsx
+++ src/app/(protected)/my-workspace/page.tsx
@@ -139,34 +139,38 @@ export default function Page() {
       </div>
       <div className="mt-6 sm:mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-12">
         {trackers?.map((tracker) => (
-          <div className="relative min-w-2xs max-w-sm hover:cursor-pointer hover:shadow-lg  transition-all duration-300">
-            <div className="absolute top-2 right-2">
+          <Card
+            key={`tracker-${tracker.id}`}
+            onClick={() => router.push(`tracker/${tracker.id}`)}
+            className="relative min-w-2xs max-w-sm hover:cursor-pointer hover:shadow-lg  transition-all duration-300"
+          >
+            <div
+              onClick={(event) => {
+                event.stopPropagation();
+              }}
+              className="absolute top-2 right-2"
+            >
               <DropdownMenuDemo />
             </div>
-            <Card
-              key={`tracker-${tracker.id}`}
-              onClick={() => router.push(`tracker/${tracker.id}`)}
-            >
-              <CardHeader>
-                <CardTitle>{tracker.title}</CardTitle>
-                <CardDescription>
-                  {tracker.description ? tracker.description : <p>&nbsp;</p>}
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="text-xs">
-                <p>
-                  {tracker.created_at.toLocaleString("en-AU", {
-                    timeZone: "Australia/Sydney",
-                  })}
-                </p>
-                <p>
-                  {tracker.updated_at.toLocaleString("en-AU", {
-                    timeZone: "Australia/Sydney",
-                  })}
-                </p>
-              </CardContent>
-            </Card>
-          </div>
+            <CardHeader>
+              <CardTitle>{tracker.title}</CardTitle>
+              <CardDescription>
+                {tracker.description ? tracker.description : <p>&nbsp;</p>}
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-xs">
+              <p>
+                {tracker.created_at.toLocaleString("en-AU", {
+                  timeZone: "Australia/Sydney",
+                })}
+              </p>
+              <p>
+                {tracker.updated_at.toLocaleString("en-AU", {
+                  timeZone: "Australia/Sydney",
+                })}
+              </p>
+            </CardContent>
+          </Card>
         ))}
       </div>
     </div>
